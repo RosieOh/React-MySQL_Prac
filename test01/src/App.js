165e922713caff43251e3c0d16bc6b426dc4ea41
RosieOh <20172207@gm.ac.kr>
2023-11-29 23:29:00 +0900

    🧞feat : react Board 외부 API Axios로 호출 방식 확인‍

---
+++ test01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import RouterPage from './components/RouterPage'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>리액트 라우터 시작</h1>
+      <RouterPage />
+      <hr />
+    </div>
+  );
+}
+
+export default App
